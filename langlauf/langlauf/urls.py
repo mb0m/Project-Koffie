07251d889f611d24295c4591fe8be7f23a683b3a
mb0m <m.matty2005@gmail.com>
2026-04-09 09:50:06 +0200

    meer basis dingen gezet

--- langlauf/langlauf/urls.py
+++ langlauf/langlauf/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path("", views.index, name="index"),
     path('admin/', admin.site.urls),
     path('base/', include('base.urls')),
     path("page1/", views.page1, name="page1")
--- langlauf/langlauf/views.py
+++ langlauf/langlauf/views.py
@@ -1,6 +1,13 @@
 # Create your views here.
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 
 
+@login_required
 def index(request):
     return render(request, "base/index.html")
+
+
+def page1(request):
+    return render(request, "page1/page1.html")
